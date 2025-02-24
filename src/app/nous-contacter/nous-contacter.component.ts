78463fa58fc2d157ed530f222a7172a21d09c01e
Jean Baptiste <jeanbaptistedielinang@gmail.com>
2025-02-24 20:34:47 +0000

    V5

--- src/app/nous-contacter/nous-contacter.component.ts
+++ src/app/nous-contacter/nous-contacter.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
 
 @Component({
   selector: 'app-nous-contacter',
-  imports: [],
+  imports: [
+    NgOptimizedImage
+  ],
   templateUrl: './nous-contacter.component.html',
   styleUrl: './nous-contacter.component.css'
 })
 export class NousContacterComponent {
-imageUrl: string = 'assets/images/r.jpg';
+imageUrl: string = 'assets/assets/r.jpg';
 }
